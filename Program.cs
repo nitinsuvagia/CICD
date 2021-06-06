196ee7dfea000ec773e9494741ed15bc9754602f
Nitin Suvagiya <viral.sojitra@softqubes.com>
2021-06-06 14:57:34 +0530

    first ci/cd project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace sample_project_dotnet_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("By NITIN");
+        }
+    }
+}
